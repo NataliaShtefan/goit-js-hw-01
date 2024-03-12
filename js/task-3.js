51a755fd4dc76e7b4efb2ca278a5f1b7fee3ecfb
NataliaShtefan <153209330+NataliaShtefan@users.noreply.github.com>
2024-03-12 23:40:52 +0100

    HM!

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,10 @@
 function getElementWidth(content, padding, border) {
-    return content + 2 * padding + 2 * border
+    // const content = (Number.parseFloat(content));
+    // const padding = (Number.parseFloat(padding));
+    // const border = (Number.parseFloat(border));
+    return Number.parseFloat(content) + Number.parseFloat(padding) +  Number.parseFloat(border) + Number.parseFloat(padding) +  Number.parseFloat(border)
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(Number.parseFloat(getElementWidth("50px", "8px", "4px")));
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px")); 

af3d82fc726d6872ded40e7f9bdec7958a2c4511
NataliaShtefan <153209330+NataliaShtefan@users.noreply.github.com>
2024-03-25 14:37:05 +0100

    tast3

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,9 @@
 function getElementWidth(content, padding, border) {
-    // const content = (Number.parseFloat(content));
-    // const padding = (Number.parseFloat(padding));
-    // const border = (Number.parseFloat(border));
-    return Number.parseFloat(content) + Number.parseFloat(padding) +  Number.parseFloat(border) + Number.parseFloat(padding) +  Number.parseFloat(border)
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
+    return totalWidth
 }
 
 console.log(getElementWidth("50px", "8px", "4px"));
